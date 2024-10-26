5a7026bd3182401f0bbe86db2c6a2eed71510bed
joserojascascopy <joserojascascopy@hotmail.com>
2024-10-26 16:47:00 -0300

    Se implemento la funcionalidad de creación para las propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,11 +1,15 @@
 <?php
+    $resultado = $_GET['resultado'] ?? null;
+
     require '../includes/funciones.php';
     addTemplate('header');
 ?>
 
     <main class="contenedor seccion">
         <h1>Administrador de Bienes Raices</h1>
-
+        <?php if(intval($resultado) === 1) : ?>
+            <p class="alerta exito">Anuncio creado correctamente</p>
+        <?php endif ?>
         <a class="boton boton-verde" href="./propiedades/crear.php">Nueva Propiedad</a>
     </main> <!-- main -->
 
